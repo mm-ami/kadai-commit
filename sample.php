2280496e8ac82bb352d234b5da3e7bdf41580e9a
m.ami <mhju.9599@gmail.com>
2020-10-12 07:15:48 +0000

    update sample.php

---
+++ sample.php
@@ -0,0 +1,18 @@
+<?php
+    $result = '無し';
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>おみくじ</title>
+    </head>
+    <body>
+        <h1>おみくじ</h1>
+        <p>おみくじの結果：<?php print htmlspecialchars($result, ENT_QUOTES, "UTF-8"); ?></p>
+        <form action="sample.php" method="POST">
+            <button type="submit" name="act" value="draw">おみくじを引く！</button>
+        </form>
+    </body>
+</html>
